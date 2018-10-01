53dbbc2bd84824128aa6396a60501db03c557984
joe <john.xu0811@gmail.com>
2018-10-01 08:35:35 -0400

    1

--- src/main/java/com/rate/callApi/callApi.java
+++ src/main/java/com/rate/callApi/callApi.java
@@ -14,7 +14,7 @@ public class callApi
 	{
 		String url = "https://stock.finance.sina.com.cn/usstock/api/jsonp_v2.php/var%20t1csbr=/US_MinlineNService.getMinline?symbol=csbr&day=1&random=1538352101654";
     callApi api = new callApi();
-    api.call(url);
+//    api(url);
 	}
 
 	
--- src/main/java/com/rate/tool/ReadUrlUtil.java
+++ src/main/java/com/rate/tool/ReadUrlUtil.java
@@ -51,6 +51,7 @@ public class ReadUrlUtil
 		{
 			return list;
 		}
+		return list;
 		
 		
 		
